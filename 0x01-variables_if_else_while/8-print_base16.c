b92d7b2a5b1dadce94fe83c9ef5748559bcc85ed
loshdami11 <loshdami@gmail.com>
2023-04-21 19:49:38 -0700

    8-print_base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ * Description: 'Print numbers of base 16'
+ * Return: Always 0
  */
-
 int main(void)
-
 {
+	int n;
+	int m;
 
-	int d;
-	char low;
-
-	for (d = '0'; d <= '9'; d++)
-	putchar(d);
-
-	for (low = 'a'; low <= 'f'; low--)
-	putchar(low);
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
 	putchar('\n');
-
 	return (0);
-
 }
 
