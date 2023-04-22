7df5acdf82cf6cbd749a0888a464c69cc68c361c
loshdami11 <loshdami@gmail.com>
2023-04-21 20:35:37 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,13 +6,16 @@
  */
 int main(void)
 {
-	int n;
+	int d;
 
-	for (n = 48; n <= 57; n++)
+	for (d = '0'; d <= '9'; d++)
 	{
-		putchar(n);
-		putchar(',');
-		putchar(' ');
+	putchar(d);
+	if (d != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}
 	}
 	putchar('\n');
 	return (0);
